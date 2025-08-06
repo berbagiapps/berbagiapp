6d54944a5ac89e3c4f1981c9f4ae75630c8186ef
hafidhuddien3 <shubuhshubuh@gmail.com>
2025-08-07 00:20:08 +0700

    (API) List Donasi Barang(As Requestor)

--- routes/donationRequests.js
+++ routes/donationRequests.js
@@ -61,5 +61,50 @@ router.post("/", authenticateUser, async (req, res) => {
 
 // Anda bisa menambahkan rute lain di sini, misalnya untuk GET semua request
 // router.get("/", ...);
+/**
+ * @route   Get /donation-requests
+ * @desc    Mengembalikan semua permintaan donasi barang
+ * @access  Public 
+ */
+router.get("/", async (req, res) => {
+  try {
+    const donationRequests = await prisma.donationRequest.findMany();
+
+    res.status(200).json({
+      message: "",
+      data: donationRequests,
+    });
+  } catch (error) {
+    console.error("Gagal Get /donation-requests:", error);
+    res.status(500).send({ message: "Terjadi kesalahan pada server." });
+  }
+});
+
+/**
+ * @route   Get /donation-requests/user
+ * @desc    Mengembalikan semua permintaan donasi barang berdasarkan user
+ * @access  Private (Harus login)
+ */
+router.get("/user", authenticateUser,async (req, res) => {
+  // Ambil ID user dari token JWT yang sudah divalidasi oleh middleware
+  const requestorFirebaseId = req.user.id;
+  // const requestorFirebaseId = "SJFRBQUF7pXxGLhYUtzT"; // dummy id for testing
+
+  try {
+    const donationRequests = await prisma.donationRequest.findMany({
+      where: {
+        requestorFirebaseId: requestorFirebaseId,
+      },
+    });
+
+    res.status(200).json({
+      message: "",
+      data: donationRequests,
+    });
+  } catch (error) {
+    console.error("Gagal Get /donation-requests/user:", error);
+    res.status(500).send({ message: "Terjadi kesalahan pada server." });
+  }
+});
 
 module.exports = router;
